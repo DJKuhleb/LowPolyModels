3d4df439727c649f23ae34fa8df7ebc27cadf512
unknown <djkuhleb@gmail.com>
2021-05-20 18:10:39 -0400

    Added models, among other unity assets

---
+++ LowPolixSciFi/Scripts/AutomaticDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticDoor : MonoBehaviour {
+
+
+    Animator doorAnimator;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        doorAnimator = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame]
+    float distanceToPlayer;
+    public float MinDistanceToOpen;
+    bool doorOpened;
+	void Update () {
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if(distanceToPlayer <= MinDistanceToOpen)
+        {
+            if (!doorOpened)
+            {
+                doorOpened = true;
+                doorAnimator.SetTrigger("Open");
+            }
+        }
+        else
+        {
+            if (doorOpened)
+            {
+                doorOpened = false;
+                doorAnimator.SetTrigger("Close");
+            }
+        }
+	}
+}
---
+++ LowPolixSciFi/Scripts/LevitateObject.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevitateObject : MonoBehaviour {
+
+	
+
+    Vector3 posOffset;
+    Vector3 tempPos;
+
+    bool inTrigger;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;   
+        levitatingObject = other.transform.gameObject;
+        if (levitatingObject.GetComponent<CharacterController>())
+            controller = levitatingObject.GetComponent<CharacterController>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        inTrigger = true;       
+        levitatingObject = other.transform.gameObject;
+        if (levitatingObject.GetComponent<CharacterController>())
+            controller = levitatingObject.GetComponent<CharacterController>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+        levitatingObject = null;
+        controller = null;       
+    }
+
+
+    GameObject levitatingObject;
+    CharacterController controller;
+    float levitationGravity = 3.0f;
+    public float LevitationFraction = 0.15f;
+
+    // Update is called once per frame
+    Vector3 vel;
+    bool pause;
+    void Update()
+    {
+
+
+        if (inTrigger)
+        {
+            if (levitatingObject)
+                if (controller)
+                {                   
+                   vel = Vector3.Lerp(transform.up * LevitationFraction, transform.up, Time.deltaTime);
+                   controller.Move(vel);                   
+                }
+        }
+
+    }
+}
